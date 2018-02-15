25ddd95f3d1a78c9bd0a3c5a4761c98fd0131445
David Nichol <david_r_nichol@yahoo.com>
2018-02-15 10:34:09 -0500

    Initial empty project

---
+++ RuachSignups/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RuachSignups.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
