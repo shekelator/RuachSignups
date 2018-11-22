acc7cad614a2f0dc3a4ce3e02e6a9b2ba93586a0
Dave Nichol <david.nichol@engie.com>
2018-11-21 21:58:41 -0500

    Added test, began move of data into code instead of json

--- Signups.Core/Besorot.fs
+++ Signups.Core/Besorot.fs
@@ -9,15 +9,43 @@ module Besorot =
     //    reading: string
     //}
 
-    //type Year = A | B | C
+    type Year = A | B | C
+
+    type ReadingRec = { Parasha: string; Reading: string }
+
+    let CycleA = [
+        {Parasha = "Parashat Bereshit"; Reading = "John 1:1–18"}
+        ]
+
+    let CycleB = [
+        {Parasha = "Parashat Bereshit"; Reading = "John 1:1–18"}
+        ]
+
+    let CycleC = [
+        {Parasha = "Parashat Bereshit"; Reading = "John 1:1–18"}
+        ]
+
+    let BesorotData (year: Year) =
+        match year with
+        | A -> CycleA
+        | B -> CycleB
+        | C -> CycleC
+        
 
     //type Readings =
     //    | year of Year
     //    | reading of list Reading
 
+    let GetYear (calendarYear: int) = 
+        match (calendarYear - 5771) % 3 with
+        | 0 -> Year.A
+        | 1 -> Year.B
+        | _ -> Year.C
 
-    let GetReading data year parasha =
-        let readings = (BesorahReadings.Parse data).A
+    let GetReading calendarYear parasha =
+        let year = GetYear calendarYear
+        let readings = BesorotData year
+
         let found = readings |> Seq.find(fun i -> i.Parasha = parasha)
         found
 
--- Signups.Test/BesorahTests.fs
+++ Signups.Test/BesorahTests.fs
@@ -5,9 +5,28 @@ open System.IO
 open Xunit
 open Signups.Core
 
-[<Fact>]
-let ``Gets items`` () =
-    let data = File.ReadAllText("./besorah-data.json")
-    let result = Besorot.GetReading data "A" "Parashat Bereshit"
+[<Theory>]
+[<InlineData(5777)>]
+[<InlineData(5778)>]
+[<InlineData(5782)>]
+let ``Bereshit always has the beginning of John`` (year) =
+    let result = Besorot.GetReading year "Parashat Bereshit"
     Assert.Equal("John 1:1–18", result.Reading)
+
+[<Theory>]
+[<InlineData(5777, "A")>]
+[<InlineData(5778, "B")>]
+[<InlineData(5779, "C")>]
+[<InlineData(5780, "A")>]
+[<InlineData(5782, "C")>]
+ let ``Chooses correct cycle based on year`` (year: int, cycleString: string) =
+    let cycle = 
+        match cycleString with
+        | "A" -> Besorot.Year.A
+        | "B" -> Besorot.Year.B
+        | _ -> Besorot.Year.C
+
+    let readingYear = Besorot.GetYear year
+    Assert.Equal(cycle, readingYear)
+
 
